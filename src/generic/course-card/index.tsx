ce917cb38c2f769f4e15d3ce986e3bf95079549f
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2025-06-17 19:46:03 +0300

    refactor: replaced no-course-image from .jpg to .svg

--- src/generic/course-card/index.tsx
+++ src/generic/course-card/index.tsx
@@ -7,7 +7,7 @@ import messages from './messages';
 import { getFullImageUrl } from './utils';
 import { DATE_FORMAT_OPTIONS } from './constants';
 
-import noCourseImg from '../../assets/no-course-image.jpg';
+import noCourseImg from '../../assets/no-course-image.svg';
 import noOrgImg from '../../assets/no-org-image.jpg';
 
 export const CourseCard = ({ course }: CourseCardProps) => {
