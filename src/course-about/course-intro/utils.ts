2d7a2265d9dc4912f9d8a7257b1a7354b5416c6e
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2025-12-03 15:24:22 +0200

    fix: fixed path to Learning MFE in getLearningHomePageUrl

--- src/course-about/course-intro/utils.ts
+++ src/course-about/course-intro/utils.ts
@@ -3,4 +3,4 @@ import { getConfig } from '@edx/frontend-platform';
 /**
  * Returns the absolute URL to the learning home page for a given course.
  */
-export const getLearningHomePageUrl = (courseId: string) => `${getConfig().LEARNING_BASE_URL}/learning/course/${courseId}/home`;
+export const getLearningHomePageUrl = (courseId: string) => `${getConfig().LEARNING_BASE_URL}/course/${courseId}/home`;
