86777f66c16c89a3249cc333901266e60c526231
Navin Karkera <navin@opencraft.com>
2026-08-07 18:33:13 +0530

    fixup! chore: improve coverage

--- src/index.tsx
+++ src/index.tsx
@@ -27,6 +27,7 @@ subscribe(APP_INIT_ERROR, (_type, data) => {
 initialize({
   messages,
   handlers: {
+    /* istanbul ignore next */
     config: () => mergeConfig({
       ENABLE_PATHWAY_PILOT_UI: process.env.ENABLE_PATHWAY_PILOT_UI === 'true',
     }),
